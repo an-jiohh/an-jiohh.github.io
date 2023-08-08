db3c973255a232f85ea8a23a4ef3c7717291edfe
an-jiohh <dkswlgh99@naver.com>
2023-08-08 18:34:23 +0900

    blog init setting

---
+++ app/blog/page.js
@@ -0,0 +1,12 @@
+import Container from "@/components/Container";
+import BlogPost from "@/components/BlogPost";
+
+export default function Blog() {
+  return (
+    <div className={`mt-10 flex flex-col`}>
+      <BlogPost />
+      <BlogPost />
+      <BlogPost />
+    </div>
+  );
+}
---
+++ components/Nav.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import navlinks from "@/data/navlinks";
+
+const Nav = () => {
+  return (
+    <nav>
+      {navlinks.map((nav) => (
+        <Link href={nav.link} key={nav.title} className={`mr-5`}>
+          {nav.title}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default Nav;
