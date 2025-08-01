c74c3ce94d0d541790ea463b1dc879ace39294c0
an-jiohh <dkswlgh99@naver.com>
2025-08-01 22:28:24 +0900

    feat(seo): 구글 사이트 검증 메타 태그 통합
    구글 사이트 검증 메타 태그를 `metadata` 객체에 통합하여 SEO 개선을 위한 설정을 간소화했습니다. 이를 통해 코드의 가독성을 높이고, 검색 엔진 최적화에 기여할 수 있습니다.

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,20 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Container from "@/components/Container";
-import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "지호의 블로그",
   description: "개발, 기술, 일상에 대한 생각을 기록하고 공유하는 공간입니다.",
+  verification: {
+    google: "LXzAKbA2-HVF1l60gXdTElFNxVVLxh1rk6qwe1V41so",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="kr">
-      <Head>
-        <meta name="google-site-verification" content="LXzAKbA2-HVF1l60gXdTElFNxVVLxh1rk6qwe1V41so" />
-      </Head>
       <body className={inter.className}>
         <Container>{children}</Container>
       </body>
